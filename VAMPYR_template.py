deb0f0e9d95e12bf84a6261fb2947114d81db3a8
Gabrielgerez <gsa017@post.uit.no>
2018-08-27 13:44:26 +0200

    updated

---
+++ VAMPYR_template.py
@@ -0,0 +1,9 @@
+from scipy import *
+from scipy import special as sp
+
+
+import vampyr3d as vp
+import numpy as np
+import matplotlib.pyplot as plt
+
+help(vp.FunctionTree)
--- one_atom_v_3d.py
+++ one_atom_v_3d.py
@@ -1,6 +1,6 @@
 from scipy import *
 from scipy import special as sp
-import math as m
+
 
 import vampyr3d as vp
 import numpy as np
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 min_scale = -4
 max_depth = 25
 order = 5
-prec = 1e-3
+prec = 1e-4
 
 corner = np.array([-1, -1, -1])
 boxes = np.array([2, 2, 2])
@@ -23,23 +23,39 @@ MRA = vp.MultiResolutionAnalysis(world, basis, max_depth)
 
 def C(x, y, z):
     eps = 80
-    s = 0.0001
+    s = 0.1
     ri = np.array([0., 0., 0.])         # position of the nucleus
     Ri = 1.20                           # *10**-10 m van der waal radius of H
 
     r = np.array([x, y, z])
     si = np.linalg.norm(r-ri) - Ri
+    C_i = 1 - 0.5*(1 + sp.erf(si/s))
 
-    return 1 - 0.5*(1 + sp.erf(si/s)) 
+    return eps*(1 - C_i) + C_i
 
 
+def rho(x, y, z):
+    alpha = 100
+    A = np.sqrt((alpha**3)/(np.pi**3))
+    return A*np.exp(-alpha*(x**2 + y**2 + z**2))
+
+
+P = vp.PoissonOperator(MRA, prec)
 
 C_tree = vp.FunctionTree(MRA)
+rho_tree = vp.FunctionTree(MRA)
+U_tree = vp.FunctionTree(MRA)
+
 vp.project(prec, C_tree, C)
+vp.project(prec, rho_tree, rho)
+vp.apply(prec, U_tree, P, rho_tree)
 
 
-x_plt = np.linspace(-2, 2, 60)
-y_plt = np.array([C_tree.evalf(i, 0, 0) for i in x_plt])
+x_plt = np.linspace(-2, 2, 100)
+C_plt = np.array([C_tree.evalf(x, 0, 0) for x in x_plt])
+U_plt = np.array([U_tree.evalf(x, 0, 0) for x in x_plt])
+# y_plt = np.array([C(x, 0, 0) for x in x_plt])
 
-plt.plot(x_plt, y_plt)
+plt.plot(x_plt, C_plt, 'r')
+plt.plot(x_plt, U_plt, 'b')
 plt.show()
